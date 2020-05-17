c6b8778ec7822ed23a9fa45b8c87e4b0e7641880
Alex Pokras <a.a.pokras@gmail.com>
2020-05-17 03:49:32 +0200

    Started programming a math expressions evaluator. Bug fixes pending.

---
+++ Miscellaneous/src/Parsing/ExpressionEvaluator.java
@@ -0,0 +1,146 @@
+package Parsing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+/**
+ * Evaluates an arithmetical expression passed in as String
+ * using Dijkstra's shunting-yard algorithm.
+ * @author alexanderpokras
+ */
+public class ExpressionEvaluator {
+	
+	private static ArrayList<String> a = new ArrayList<String> (Arrays.asList( 
+			new String[]{
+					"-","3","*","(","2","*","sqrt(","2","^","2",")","-","6",")"
+	}));	//for testing
+	
+	private static Stack<Operator> ops = new Stack<Operator>();
+	private static Stack<Double> vals = new Stack<Double>();
+	
+	/**
+	 * @return true if the parameter is a digit, a comma or a point
+	 */
+	private static boolean isPartOfNumber(char c) {
+		if (c==',') c='.';
+		return Character.isDigit(c)||c=='.';
+	}
+	
+	/**
+	 * Parses the expression into the <i><b>ArrayList<String> a</i></b> of numbers and operators,
+	 * each in a separate entry.
+	 */
+	//TODO change (( to (*( and N( to N*(
+	//TODO (more importantly) change (-( to ( -1 * ( and (-oper( to ( -1 * oper(
+	private static void parseExpression(String expression) {
+
+		a = new ArrayList<String>(1);
+		char[] charArray = expression.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		int i=0;
+		if (charArray[0]=='-') a.add(i++,"0");
+		sb.append(charArray[0]);
+		for (int j=1; j<charArray.length;j++) {
+			char curr = charArray[j];
+			char prev = charArray[j-1];
+
+			//starting a new entry
+			if ("()+-*/".indexOf(prev)>-1 ||
+					(isPartOfNumber(prev)^isPartOfNumber(curr))
+					) {
+				a.add(i,sb.toString());
+				i++;
+				sb = new StringBuilder();	
+			}
+			sb.append(curr);
+		}
+		if(!sb.toString().isEmpty()) a.add(sb.toString());
+	}
+	
+	/**
+	 * Returns true if the String parameter can be parsed to a Double.
+	 */
+	private static boolean isNumeric(String s) {
+		try {
+			Double.parseDouble(s);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Process the last operator:
+	 * Pop the operator off <i><b>ops</i></b>, process the top two values in <i><b>vals</i></b>
+	 * and push the result on <i><b>vals</i></b>.
+	 */
+	private static void processTopOfOps() {
+		double d = ops.pop().processTwoArgs(vals.pop(), vals.pop());
+		vals.push(d);
+	}
+	
+	/**
+	 * Evaluate a math expression from the <i><b>ArrayList<String> a</i></b>
+	 * in which each number and each operation (including parentheses)
+	 * is in a separate entry
+	 * @return Double result of evaluation
+	 */
+	private static double evaluateArrayList() {
+		for (String s : a) {
+			System.out.println(vals.toString());
+			System.out.println(ops.toString());
+			//push a new Double onto vals
+			if (isNumeric(s)) vals.push(Double.parseDouble(s));
+			//work with a new operator
+			else {
+				Operator newOp = new Operator(s);
+				//System.out.println(newOp);
+				while (true) {
+					//work with right parenthesis:
+					if (newOp.isRightParen()) {
+						System.out.print("Working with )... ");
+						while(!ops.peek().isLeftParen()) {
+							processTopOfOps();
+						}
+						double val = vals.pop();
+						val = ops.pop().processOneArg(val);
+						System.out.println(val);
+						vals.push(val);
+						System.out.println(val);
+						break;
+					}
+					//pushing the new operator onto the operator stack
+					if (ops.isEmpty() || newOp.getPrecedence()>ops.peek().getPrecedence()
+							|| newOp.isLeftParen()) { 
+						ops.push(newOp); 
+						break;
+					//processing operators in ops stack until we get to
+					//an operator whose precedence is <= newOp or
+					//until ops is empty
+					} else {
+						processTopOfOps();
+					}
+				}
+			}
+		}
+		while (!ops.isEmpty()) {
+			processTopOfOps();
+		}
+		return vals.get(0);
+	}
+	
+	public static double evaluate(String expression) {
+		parseExpression(expression);
+		return evaluateArrayList();
+	}
+	
+	public static void main(String[] args) {
+		//ExpressionEvaluator ee = new ExpressionEvaluator();
+		//System.out.println("Answer: "+evaluateArrayList());
+		parseExpression("-10*(sqrt(2^2+12)-(3+1))");
+		System.out.println(a.toString());
+		System.out.println("Answer: "+evaluateArrayList());
+
+	}
+
+}
---
+++ Miscellaneous/src/Parsing/Operator.java
@@ -0,0 +1,96 @@
+package Parsing;
+/**
+ * A class to represent a mathematical operator.
+ * Operators with bigger precedence will be evaluated first.
+ * @author alexanderpokras
+ *
+ */
+class Operator {
+	
+	String opString;
+	private int precedence;
+	/**
+	 * Determine precedence
+	 * @param opString String that represents an operator in the expression
+	 */
+	Operator(String opString){
+		this.opString=opString;
+		switch(opString) {
+		case("+") : precedence=1; break;
+		case("-") : precedence=1; break;
+		case("*") : precedence=2; break;
+		case("/") : precedence=2; break;
+		case("^") : precedence=3; break;
+//		case("("):break;
+//		case(")"):break;
+//		default: throw new IllegalArgumentException(opString+" is not a supported operation");
+		}
+	}
+	
+	/**
+	 * Perform an operation according to the operator's type
+	 * for operators that take two arguments
+	 * (e.g., addition for "+", subtraction for "-" etc.)
+	 * @param val2
+	 * @param val1
+	 * @return result of the operation
+	 */
+	double processTwoArgs(double val2, double val1) {
+		switch(opString) {
+		case("+") : return val1+val2;
+		case("-") : return val1-val2;
+		case("*") : return val1*val2;
+		case("/") : return val1/val2;
+		case("^") : return Math.pow(val1, val2);
+		default: throw new UnsupportedOperationException(opString+" is an unknown operation");
+		}
+	}
+	
+	/**
+	 * Perform an operation according to the operator's type
+	 * for operators that take one argument and have a ( at their end
+	 * (e.g., square root for "sqrt(" )
+	 * @param val
+	 * @return result of the operation
+	 */
+	double processOneArg(double val) {
+		switch(opString) {
+		case("(") : return val;
+		//case("-(") : return -val;
+		case("sin(") : return Math.sin(val);
+		case("cos(") : return Math.cos(val);
+		case("tan(") :
+		case("tg(") : return Math.tan(val);
+		case("cot(") :
+		case("ctg(") : return 1/Math.tan(val);
+		case("arcsin(") : return Math.asin(val);
+		case("arccos(") : return Math.acos(val);
+		case("arctan") : 
+		case("arctg(") : return Math.atan(val);
+		case("arccot(") :
+		case("arcctg(") : return Math.atan(1/val);
+		case("sqrt(") : return Math.sqrt(val);
+
+
+		default: throw new UnsupportedOperationException(opString+" is an unknown operation");	
+		}
+	}
+	
+	
+	int getPrecedence() {
+		return precedence;
+	}
+	
+	boolean isLeftParen() {
+		return opString.endsWith("(");
+	}
+	
+	boolean isRightParen() {
+		return opString.equals(")");
+	}
+	
+	@Override
+	public String toString() {
+		return opString;
+	}
+}
