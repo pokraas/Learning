e8da3c8095eb0f3f7741fef4eb47224f781961a2
Alex Pokras <a.a.pokras@gmail.com>
2021-02-06 10:30:43 +0100

    Task E from the 2nd stage of the UA Olympiad for 9th grade

--- Miscellaneous/src/Miscellaneous/UAOlympiadE.java
+++ Miscellaneous/src/Miscellaneous/UAOlympiadE.java
@@ -1,16 +1,18 @@
 package Miscellaneous;
 
+import java.util.Arrays;
+
 public class UAOlympiadE {
-	//n2 = 2 * n, v should already be sorted
-	int[] b(int[] v, int n2) {
-		int[] b = new int[n2];
+	//v should already be sorted, v should be of length n
+	static int[] b(int[] v, int n) {
+		int[] b = new int[n];
 		int j = 0; //index of the next free space in b
 		for (int c = 1; c < v[0]; c++) {
 			b[j] = c;
 			j++;
 		}
 		
-		for (int i = 1; i < n2; i++) {
+		for (int i = 1; i < n; i++) {
 			for (int c = v[i - 1] + 1; c < v[i]; c++) {
 				b[j] = c;
 				j++;
@@ -18,7 +20,7 @@ public class UAOlympiadE {
 			
 		}
 		
-		for (int c = v[n2 - 1] + 1; c < n2; c++) {
+		for (int c = v[n - 1] + 1; c <= 2 * n; c++) {
 			b[j] = c;
 			j++;
 		}
@@ -26,6 +28,6 @@ public class UAOlympiadE {
 	}
 	
 	public static void main(String[] args) {
-		
+		System.out.println(Arrays.toString(b(new int[]{4, 5, 6},3)));
 	}
 }
