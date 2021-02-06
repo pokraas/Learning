b4493b696338305a3974cb38f15f7f24fb57f894
Alex Pokras <a.a.pokras@gmail.com>
2021-02-06 10:40:38 +0100

    Task E from the 2nd stage of the UA Olympiad for 9th grade

--- Miscellaneous/src/Miscellaneous/UAOlympiadE.java
+++ Miscellaneous/src/Miscellaneous/UAOlympiadE.java
@@ -27,7 +27,26 @@ public class UAOlympiadE {
 		return b;
 	}
 	
+	//Maximize the points for w (winner), return how many points the winner gets.
+	//The winner and loser arrays should be already sorted and of length n.
+	static int maximize(int[] w, int[] l, int n) {
+		int p = 0; //winner's points
+		int j = 0; //index in l
+		for (int i = 0; i < n; i++) { //i - index in w
+			if (w[i] > l[j]) {
+				j++;
+				p++;
+			}
+		}
+		return p;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(b(new int[]{4, 5, 6},3)));
+		int n = 3;
+		int[] vcard = new int[]{4, 5, 6};
+		int[] bcard = b(vcard,n);
+		System.out.println(Arrays.toString(bcard));
+		System.out.println();
+		System.out.println(maximize(vcard, bcard, n));
 	}
 }
